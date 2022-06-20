d6386d080c83d7659828892cee4d3e5044c79968
bangdonghyuk <yolo0528@naver.com>
2022-06-20 17:07:20 +0900

    Updates

---
+++ precache-manifest.21e27831841f0d3e72c18a8464f12526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fcbfc7bc716d13a1920",
+    "url": "/movie_app_2019/static/css/main.5cfa3781.chunk.css"
+  },
+  {
+    "revision": "4fcbfc7bc716d13a1920",
+    "url": "/movie_app_2019/static/js/main.4fcbfc7b.chunk.js"
+  },
+  {
+    "revision": "ad1de07ae56dfaf4f24b",
+    "url": "/movie_app_2019/static/js/1.ad1de07a.chunk.js"
+  },
+  {
+    "revision": "9a11dd6fd8f06cb0b4a7",
+    "url": "/movie_app_2019/static/js/runtime~main.9a11dd6f.js"
+  },
+  {
+    "revision": "ed201a6f75ede5ffe0e772334a004bf8",
+    "url": "/movie_app_2019/index.html"
+  }
+];
